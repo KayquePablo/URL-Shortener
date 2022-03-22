a1a648a8006292c95d0970fa6064df303566eccf
Kayque Pablo <92043829+KayquePablo@users.noreply.github.com>
2022-03-22 09:58:10 -0300

    Update src/config/Constants.ts

--- src/config/Constants.ts
+++
@@ -1,5 +0,0 @@
-export const config = {
-	API_URL: 'http://localhost:3000',
-	MONGO_CONNECTION:
-		'mongodb+srv://KayquePablo:<password>@url-shortener.jdotv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-}
